c5384c3dca0c1513154ebc14db70dbfbf3e3c2ae
Xiaobiao.Wu <Xiaobiao.Wu@geely.com>
2018-02-11 14:49:08 +0800

    添加1号题目

--- src/main/java/com/leetcode/algorithms/start/resolution/TwoSum.java
+++ src/main/java/com/leetcode/algorithms/start/resolution/TwoSum.java
@@ -3,7 +3,24 @@ package com.leetcode.algorithms.start.resolution;
 public class TwoSum {
 
 
+    public int[] twoSum(int[] nums, int target) {
 
+
+        for(int i =0; i<nums.length; i++) {
+            for (int j=i+1;j<nums.length;j++) {
+                if (nums[i] + nums[j] == target) {
+
+                    int[] reslt = new int[2];
+                    reslt[0] = i;
+                    reslt[1] = j;
+
+                    return reslt;
+                }
+            }
+        }
+        return null;
+
+    }
 
 
 }
